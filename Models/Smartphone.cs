4feac762804053475ef2e989475d664c6d86d109
vinicius prado <viniciuspra@gmail.com>
2024-01-05 12:04:22 -0300

    Desafio Smartphone curso C# DIO

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,13 +3,17 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
-        {
-            Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
-        }
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int Memória { get; set; }
+        
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+    {
+        Numero = numero;
+        Modelo = modelo;
+        IMEI = imei;
+        Memória = memoria;
+    }
 
         public void Ligar()
         {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿using DesafioPOO.Models;
+﻿﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia smartphone1 = new(numero: "419860567", modelo: "G60 5G", imei: "12346578bc", memoria: 256);
+Iphone smartphone2 = new(numero: "4199506412", modelo: "Iphone 15", imei: "12346574987fds", memoria: 1024);
+
+Console.WriteLine("Smartphone Nokia:");
+smartphone1.Ligar();
+smartphone1.ReceberLigacao();
+smartphone1.InstalarAplicativo(nomeApp: "Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphoe Iphone:");
+smartphone1.Ligar();
+smartphone2.ReceberLigacao();
+smartphone2.InstalarAplicativo(nomeApp: "Signal");
